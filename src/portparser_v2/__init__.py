f1c4d2473a952b2ce2fb5a792b305abcf7dee769
Leo Chatain <leochatain@gmail.com>
2025-12-05 11:19:38 +0000

    Create importable project.

---
+++ src/portparser_v2/__init__.py
@@ -0,0 +1,7 @@
+"""Portparser v2 - A parsing model for Brazilian Portuguese."""
+
+from portparser_v2.core import parse, parse_text, parse_file
+
+__version__ = "0.1.0"
+__all__ = ["parse", "parse_text", "parse_file"]
+
